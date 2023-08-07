34f14d2388fbb67d36777e638adeefe0f9d79ca5
metaspook <metaspook@gmail.com>
2023-08-07 15:03:28 +0600

    update page comments

--- lib/modules/comments/view/comments_page.dart
+++ lib/modules/comments/view/comments_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:su_thesis_book/modules/comments/comments.dart';
 import 'package:su_thesis_book/shared/shared.dart';
+import 'package:su_thesis_book/utils/utils.dart';
 
 class CommentsPage extends StatelessWidget {
   const CommentsPage({super.key});
@@ -9,43 +10,52 @@ class CommentsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
-      // extendBody: true,
-      resizeToAvoidBottomInset: true,
       appBar: AppBar(
         leading: Mod.backButton(context),
         centerTitle: true,
         title: const Text('Comments'),
       ),
-      body: ListView.builder(
-        // shrinkWrap: true,
-        physics: const BouncingScrollPhysics(),
-        padding: const EdgeInsets.all(5),
-        itemCount: 20 + 1,
-        itemBuilder: (context, index) {
-          final comment = Comment(
-            author: 'author',
-            profileImagePath: 'profileImagePath',
-            content: 'content',
-            createdAt: DateTime.now(),
-          );
-          return index == 0
-              ? Container(height: kToolbarHeight * 1.4)
-              : CommentCard(comment);
-        },
+      body: Stack(
+        children: [
+          ListView.builder(
+            shrinkWrap: true,
+            physics: const BouncingScrollPhysics(),
+            padding: const EdgeInsets.all(5),
+            itemCount: 20,
+            itemBuilder: (context, index) {
+              final comment = Comment(
+                author: 'author',
+                profileImagePath: 'profileImagePath',
+                content: 'content',
+                createdAt: DateTime.now(),
+              );
+              return index == 0
+                  ? Container(height: kToolbarHeight * 1.4)
+                  : CommentCard(comment);
+            },
+          ),
+          Column(
+            children: [
+              const Spacer(),
+              ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  topRight: Radius.circular(24),
+                  topLeft: Radius.circular(24),
+                ),
+                child: Card(
+                  color:
+                      context.theme.colorScheme.inversePrimary.withOpacity(.75),
+                  child: const Padding(
+                    padding:
+                        EdgeInsets.only(bottom: 12, left: 17.5, right: 17.5),
+                    child: TextField(),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
 }
-  //  ClipRRect(
-  //           borderRadius: const BorderRadius.only(
-  //             topRight: Radius.circular(24),
-  //             topLeft: Radius.circular(24),
-  //           ),
-  //           child: Card(
-  //             color: context.theme.colorScheme.inversePrimary.withOpacity(.75),
-  //             child: const Padding(
-  //               padding: EdgeInsets.only(bottom: 12, left: 17.5, right: 17.5),
-  //               child: TextField(),
-  //             ),
-  //           ),
-  //         ),
